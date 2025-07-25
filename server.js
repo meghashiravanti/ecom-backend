f3a58911349678a6693e11841664e18de5c64ffa
klebcabailhongal <meghashiravanti161@gamile.com>
2025-07-25 15:43:09 +0530

    Authentication

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/", productRoutes);
+app.use("/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL)
@@ -18,8 +20,6 @@ mongoose
     });
   })
 
-  .catch(() => {
-    console.log("Error connecting to database:", error.message);
+  .catch((error) => {
+    console.log("Error connecting to database", error.message);
   });
-
-module.exports;
